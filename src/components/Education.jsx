06f9300ad51041a23cac128cd29ea4adb70dcd2a
Juan Quintana <juan.quintana5@upr.edu>
2023-07-19 23:32:57 -0400

    Added Scroll Animations and Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import "./css/Education.css";
+import { Fade } from "react-awesome-reveal";
+const myEducation = [
+  {
+    school: "University of Puerto Rico - Mayaguez",
+    degree: "B.S. Computer Science and Engineering",
+    years: "2021 - 2026(Exp.)",
+    gpa: "3.77",
+    courses:
+      "Data Structures, Advanced Programming(OOP), Computing Fundamentals",
+  },
+];
 
 export const Education = () => {
   return (
-    <section className="EdContainer w-full h-[100vh] border-t border-t-neutral-700">
-      <div className="backdrop-brightness-50 w-full h-full text-white">
+    <section className="EdContainer w-full border-t border-t-neutral-700 flex flex-col justify-center">
+      <h1 className="text-white text-2xl font-bold text-center m-4">
         Education
-      </div>
+      </h1>
+      {myEducation.map((education) => {
+        return (
+          <Fade className="self-center" direction="up">
+            <div className="text-black flex flex-col justify-center items-center bg-neutral-100 border border-neutral-600 rounded-xl md:w-96 p-4 sm:mx-2 md:m-0 mt-4 mb-8">
+              <div>
+                <h2 className=" text-xl font-bold md:whitespace-nowrap">
+                  {education.degree}
+                </h2>
+                <h4 className="font-thin italic">{education.years}</h4>
+              </div>
+              <div>
+                <h3 className="text-start">{education.school}</h3>
+                <h4 className="text-center">GPA: {education.gpa}</h4>
+              </div>
+              <h4 className=" text-left">
+                <span className="underline font-semibold">
+                  Relevant Courses:
+                </span>{" "}
+                {education.courses}
+              </h4>
+            </div>
+          </Fade>
+        );
+      })}
     </section>
   );
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./css/About.css";
 import { experiences } from "../data/experiences";
+import { Fade, AttentionSeeker } from "react-awesome-reveal";
 export const Experience = () => {
   return (
-    <section className="text-white w-full bg-black">
+    <section className="text-white w-full bg-black overflow-hidden">
       <h1
         id="About"
         className="w-full flex justify-center items-center p-6 font-sans font-bold text-3xl"
@@ -12,47 +13,53 @@ export const Experience = () => {
       </h1>
       <hr className="w-full border-[1px] border-white" />
       <div className="flex flex-col justify-center w-full items-center p-8">
-        <div className="grid grid-cols-2 gap-10 m-4 w-full lg:px-20">
-          <h2 className="text-2xl font-sans font-bold w-full text-start">
-            Expirience:
+        <div className="flex-col justify-center place-items-center flex gap-10 m-4 w-full">
+          <h2 className="text-2xl font-sans font-bold w-full text-center">
+            Experience:
           </h2>
           {experiences.map((experience) => {
             let classEven = "lg:col-start-1";
+            let direction = "left";
             if (experience.id % 2 === 0) {
               classEven = "lg:col-start-2";
+              direction = "right";
             }
             return (
-              <div
+              <Fade
                 key={experience.id}
-                className={` col-span-2 md:grid md:grid-cols-2 shadow-md shadow-neutral-700 bg-black border border-neutral-700 p-4 rounded-xl lg:w-[800px] ${classEven} `}
+                direction={direction}
+                className={`  shadow-md shadow-neutral-700 bg-black border border-neutral-700 p-4 rounded-xl lg:w-[800px] ${classEven} `}
               >
-                <div className="col-start-1 md:border-r-neutral-700 md:border-r">
-                  <h3 className=" font-sans font-bold w-full text-start">
-                    <a
-                      className="hover:underline text-xl hover:text-blue-400"
-                      href={experience.link}
-                    >
-                      {experience.title}
-                    </a>
-                  </h3>
-                  <h4 className="italic font-thin text-sm">
-                    {experience.company}
-                  </h4>
-                  <p className="italic font-thin text-sm">
-                    {experience.period}
-                  </p>
-                  <p className="py-3 mr-6 mb-6  md:mb-0">
-                    {experience.description}
-                  </p>
+                <div className="col-span-2 md:grid md:grid-cols-2">
+                  <div className="col-start-1 md:border-r-neutral-700 md:border-r">
+                    <h3 className=" font-sans font-bold w-full text-start">
+                      <a
+                        className="hover:underline text-xl hover:text-blue-400"
+                        href={experience.link}
+                        target="_blank"
+                      >
+                        {experience.title}
+                      </a>
+                    </h3>
+                    <h4 className="italic font-thin text-sm">
+                      {experience.company}
+                    </h4>
+                    <p className="italic font-thin text-sm">
+                      {experience.period}
+                    </p>
+                    <p className="py-3 mr-6 mb-6  md:mb-0">
+                      {experience.description}
+                    </p>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <img
+                      className="w-72 rounded-xl"
+                      src={experience.img}
+                      alt={experience.title}
+                    />
+                  </div>
                 </div>
-                <div className="flex justify-center items-center">
-                  <img
-                    className="w-72 rounded-xl"
-                    src={experience.img}
-                    alt={experience.title}
-                  />
-                </div>
-              </div>
+              </Fade>
             );
           })}
         </div>
