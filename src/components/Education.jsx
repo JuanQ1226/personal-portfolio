d5096bb6fe5193b61f549d1c4da224faa720c0bf
Juan Quintana <juan.quintana5@upr.edu>
2023-07-19 23:40:12 -0400

    fixed margin issue

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -21,7 +21,7 @@ export const Education = () => {
       {myEducation.map((education) => {
         return (
           <Fade className="self-center" direction="up">
-            <div className="text-black flex flex-col justify-center items-center bg-neutral-100 border border-neutral-600 rounded-xl md:w-96 p-4 sm:mx-2 md:m-0 mt-4 mb-8">
+            <div className="text-black flex flex-col justify-center items-center bg-neutral-100 border border-neutral-600 rounded-xl md:w-96 p-4 sm:mx-2 md:mx-0 mt-4 mb-8">
               <div>
                 <h2 className=" text-xl font-bold md:whitespace-nowrap">
                   {education.degree}
