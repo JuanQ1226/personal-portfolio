eea0ab9f028a59f38c5fd697f0815785b96e8da1
Juan Quintana <juan.quintana5@upr.edu>
2023-10-05 20:03:44 -0400

    Imporved Cards

--- src/components/pieces/ProjectCard.jsx
+++ src/components/pieces/ProjectCard.jsx
@@ -4,9 +4,9 @@ export const ProjectCard = ({ title, image, description, link }) => {
   return (
     <div
       onClick={() => window.open(link)}
-      className="my-4 md:my-0 border border-gray-500 hover:scale-110 transition-all ease-in-out text-black flex flex-col justify-center sm:w-auto lg:w-[70%] items-center p-4 rounded-xl shadow-xl shadow-gray-600 bg-white"
+      className="my-4 border-gray-500 hover:scale-110 transition-all ease-in-out text-black flex flex-col justify-center sm:w-auto lg:w-[70%] items-center p-2 rounded-xl shadow-xl shadow-gray-600 bg-white"
     >
-      <img src={image} className="h-50 w-auto" />
+      <img src={image} className="h-50 w-auto border border-black rounded-xl" />
       <div className="flex justify-center flex-col items-center">
         <h2 className="text-xl font-semibold">{title}</h2>
         <p className="text-center">{description}</p>
