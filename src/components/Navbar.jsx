2822819d52caeb676a307b8ab6e509f58b030832
Juan Quintana <juan.quintana5@upr.edu>
2023-10-11 16:02:44 -0400

    Tweaked Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Navbar.css";
 
 const NavLinks = [
@@ -8,9 +8,15 @@ const NavLinks = [
 ];
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="sticky top-0 left-0 w-full z-50">
-      <nav className="bg-black border-b-[1px] border-b-neutral-700 text-white flex justify-between p-4 items-center">
+      <nav className="hidden  bg-black border-b-[1px] border-b-neutral-700 text-white md:flex justify-between p-4 items-center">
         <div className="flex justify-center items-center text-center">
           <h1 className="font-nav hidden md:block md:text-2xl">
             Juan.Quintana
@@ -42,6 +48,40 @@ export const Navbar = () => {
           })}
         </ul>
       </nav>
+
+      <nav className="flex md:hidden bg-black border-b-[1px] border-b-neutral-700 text-white justify-between p-4">
+        <h1 className="font-nav md:hidden block md:text-2xl">Juan.Quintana</h1>
+        <button onClick={toggleMenu} className="mr-2">
+          {menuOpen ? (
+            <i className="fa-solid fa-xmark"></i>
+          ) : (
+            <i className="fa-solid fa-bars"></i>
+          )}
+        </button>
+        {menuOpen && (
+          <div className="md:hidden">
+            <div className="bg-black flex flex-col gap-4 pb-4">
+              {NavLinks.map((link) => {
+                const handleClick = () => {
+                  const element = document.getElementById(link.text);
+                  element.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest",
+                  });
+                };
+                return (
+                  <button className="flex justify-between">
+                    {<i onClick={handleClick} className={link.icon}></i>}
+                    {link.text}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </nav>
+
       <div title="My Resume" className="fixed bottom-[28px] right-[8px] ">
         <div className="mb-10 hover:scale-110 transition-all ease-in-out duration-300">
           <a
