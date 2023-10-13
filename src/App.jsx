fc322e0fbab0608ee72293e150d1ba55416fc665
Juan Quintana <juan.quintana5@upr.edu>
2023-10-13 00:38:16 -0400

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
+import { inject } from "@vercel/analytics/";
+
+inject();
 
 function App() {
   return (
