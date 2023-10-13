cb002ad1da76944023bbfee3759a38857faeacd2
Juan Quintana <juan.quintana5@upr.edu>
2023-10-13 00:41:20 -0400

    Removed Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import "./App.css";
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
-import { inject } from "@vercel/analytics/";
-
-inject();
 
 function App() {
   return (
